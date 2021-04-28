c1d8b969fe67c0d9893dee80e7486d05e2bd605a
n0nu <mechat0141@gmail.com>
2021-04-28 14:33:53 +0530

    exception handling added

--- controllers/yahoo-finance.controller.js
+++ controllers/yahoo-finance.controller.js
@@ -10,22 +10,26 @@ export const getNews = async (req, res) => {
   const {
     query: { q, region },
   } = req;
-
+  let response;
   // q is required param
   if (!q) {
     return res.status(400).send("Parameter 'q' is required");
   }
 
-  const response = await axios({
-    method: "GET",
-    url: `https://${process.env.API_HOST}/auto-complete`,
-    headers: {
-      "x-rapidapi-key": process.env.API_KEY,
-      "x-rapidapi-host": process.env.API_HOST,
-      useQueryString: true,
-    },
-    params: { q, ...(region && { region }) },
-  });
+  try {
+    response = await axios({
+      method: "GET",
+      url: `https://${process.env.API_HOST}/auto-complete`,
+      headers: {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": process.env.API_HOST,
+        useQueryString: true,
+      },
+      params: { q, ...(region && { region }) },
+    });
+  } catch (err) {
+    return res.status(403).send(err.message);
+  }
 
   return res.send(response.data);
 };
@@ -40,22 +44,27 @@ export const getAnalysis = async (req, res) => {
   const {
     query: { symbol, region },
   } = req;
+  let response;
 
   // symbol is required param
   if (!symbol) {
     return res.status(400).send("Parameter 'symbol' is required");
   }
 
-  const data = await axios({
-    method: "GET",
-    url: `https://${process.env.API_HOST}/stock/v2/get-analysis`,
-    headers: {
-      "x-rapidapi-key": process.env.API_KEY,
-      "x-rapidapi-host": process.env.API_HOST,
-      useQueryString: true,
-    },
-    params: { symbol, ...(region && { region }) },
-  });
+  try {
+    response = await axios({
+      method: "GET",
+      url: `https://${process.env.API_HOST}/stock/v2/get-analysis`,
+      headers: {
+        "x-rapidapi-key": process.env.API_KEY,
+        "x-rapidapi-host": process.env.API_HOST,
+        useQueryString: true,
+      },
+      params: { symbol, ...(region && { region }) },
+    });
+  } catch (err) {
+    return res.status(403).send(err.message);
+  }
 
-  return res.send(data.data);
+  return res.send(response.data);
 };
